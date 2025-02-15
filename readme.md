969f024f2002fb0363da8645de4582ab271eebb0
Caramel4764 <nina.dong4764@gmail.com>
2025-02-14 23:07:19 -0800

    fix: translate instead of left

--- readme.md
+++ readme.md
@@ -1,3 +1,11 @@
 <a href="https://www.vecteezy.com/free-photos/bubble-tea">Bubble Tea Stock photos by Vecteezy</a>
 
 <a href="https://www.vecteezy.com/free-photos/bubble-tea">Bubble Tea Stock photos by Vecteezy</a>
+
+Photo by <a href="https://unsplash.com/@jeremyyappy?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Jeremy Yap</a> on <a href="https://unsplash.com/photos/selective-focus-photography-of-latte-in-teacup-jn-HaGWe4yw?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+      
+
+Photo by <a href="https://unsplash.com/@teacora?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">TeaCora Rooibos</a> on <a href="https://unsplash.com/photos/clear-drinking-glass-with-orange-liquid-FnTWsBohkdo?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+
+Photo by <a href="https://unsplash.com/@frokz?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Dani</a> on <a href="https://unsplash.com/photos/brown-bread-on-white-ceramic-plate-CLtLGfF6mwI?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+      
--- script.js
+++ script.js
@@ -1,27 +1,68 @@
-const floatCaroImg = document.querySelectorAll('.slide-anim-img');
+//const floatCaroImg = document.querySelectorAll('.slide-anim-img');
 const pauseBtn = document.getElementById("pause");
+let floatCaroImg = [];
 const floatCaroDiv = document.querySelectorAll('.float-caro-div');
-
-for (let i = 0; i<floatCaroImg.length;i++) {
-  floatCaroImg[i].style.left=`${i*360}px`
+const reel = document.querySelectorAll('.reel')[0];
+let imageReel = [
+  {
+    src:"./img/mint-boba.png",
+  },
+  {
+    src:"./img/multi-color.jpg",
+  },
+  {
+    src:"./img/creme-bun.jpg",
+  },
+  {
+    src:"./img/orange-tea.jpg",
+  },
+  {
+    src:"./img/small-coffee.jpg",
+  },
+]
+for (let i = 0; i<imageReel.length;i++) {
+  let imgDiv = document.createElement('div');
+  let caroImg = document.createElement('img');
+  caroImg.classList.add('slide-anim-img');
+  caroImg.src=imageReel[i].src;
+  caroImg.style.transform=`translate(${i*360}px, 0)`
+  imgDiv.appendChild(caroImg);
+  imgDiv.classList.add('float-caro-div');
+  reel.appendChild(imgDiv);
+  floatCaroImg.push(caroImg)
 }
 const floatCaro = function(){
   let data = {
     isMoving: true,
   }
+  //return translate x value
+  function calcTransX(element) {
+    const style = window.getComputedStyle(element)
+    const matrix = style.transform;
+    const matrixValues = matrix.match(/matrix.*\((.+)\)/)[1].split(', ');
+    return matrixValues[4];
+  }
   function handleFloat () {
     if (data.isMoving) {
       for (let i = 0; i<floatCaroImg.length;i++) {
-        if (parseInt(floatCaroImg[i].style.left,10) < (0-300)) {
+        if (calcTransX(floatCaroImg[i]) < (0-300)) {
           let prev;
           if (i==0) {
-            prev = parseInt(floatCaroImg[floatCaroImg.length-1].style.left,10)+360;
+            prev = calcTransX(floatCaroImg[floatCaroImg.length-1])+360;
+            console.log({
+              floatCaro:floatCaroImg[floatCaroImg.length-1],
+              calcTransX: prev
+            })
           } else {
-            prev = parseInt(floatCaroImg[i-1].style.left,10)+360;
+            prev = calcTransX(floatCaroImg[i-1])+360;
+            console.log({
+              floatCaro: floatCaroImg[i-1],
+              calcTransX: prev
+            })
           }
-          floatCaroImg[i].style.left=`${prev}px`;
+          floatCaroImg[i].style.transform=`translate(${prev}px, 0)`;
         } else {
-          floatCaroImg[i].style.left=`${parseInt(floatCaroImg[i].style.left, 10)-1}px`;
+          floatCaroImg[i].style.transform=`translate(${calcTransX(floatCaroImg[i])-5}px, 0`;
         }
       }
     }
@@ -47,5 +88,5 @@ pauseBtn.addEventListener('click', function() {
 })
 setInterval(function(){
   floatCaro.handleFloat();
-}, 15)
+}, 1500)
 
